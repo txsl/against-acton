1c83d92f91c114a20ef75fe3166648487901d6bb
Thomas Lim <txl11@ic.ac.uk>
2013-02-26 21:33:28 +0000

    Which idiot left all of that code commented out?

--- index.php
+++ index.php
@@ -5,32 +5,32 @@ require __DIR__.'/src/bootstrap.php';
 if($_SERVER['REQUEST_METHOD'] == 'POST')
 {
 	$data = $_POST;
-	// if(NoCSRF::check('notrolling', $data))
-	// {
-	// 	if(!array_key_exists('anon', $data))
-	// 	{
-	// 		$data['anon'] = 0;
-	// 	}
-	// 	$result = $signatures->insert($data);
-	// 	if($result[0])
-	// 	{
-	// 		// Render good news page
-	// 		echo $twig->render('thanks.html.twig');
-	// 		session_destroy(); //No need to keep csrf hash any more
-	// 	}
-	// 	else
-	// 	{
-	// 		//Render error (and new CSRF key)
-	// 		$csrfkey = NoCSRF::generate('notrolling');
-	// 		echo $twig->render('landing.html.twig', array('error' => $result[1], 'data'=>$data, 'csrfkey'=>$csrfkey));
-	// 	}
-	// }
-	// else
-	// {
+	if(NoCSRF::check('notrolling', $data))
+	{
+		if(!array_key_exists('anon', $data))
+		{
+			$data['anon'] = 0;
+		}
+		$result = $signatures->insert($data);
+		if($result[0])
+		{
+			// Render good news page
+			echo $twig->render('thanks.html.twig');
+			session_destroy(); //No need to keep csrf hash any more
+		}
+		else
+		{
+			//Render error (and new CSRF key)
+			$csrfkey = NoCSRF::generate('notrolling');
+			echo $twig->render('landing.html.twig', array('error' => $result[1], 'data'=>$data, 'csrfkey'=>$csrfkey));
+		}
+	}
+	else
+	{
 		//Render error (and new CSRF key)
 		$csrfkey = NoCSRF::generate('notrolling');
 		echo $twig->render('landing.html.twig', array('error' => 'Yikes! Our CSRF (Request Forgery Protection) system tripped. If this keeps happening, please <a href="mailto:thomas.lim11@imperial.ac.uk">let us know</a>.', 'data'=>$data, 'csrfkey'=>$csrfkey));
-	//}
+	}
 }
 else
 {
